b7a0ab437bd68b09bed99a51859a149ab87a4c49
Katie Morton <kmalleena@gmail.com>
2022-11-28 04:05:55 -0500

    first full non-testing upload & readme updates

--- README.md
+++ README.md
@@ -1,5 +1,60 @@
 # hangman
 A hangman game created with Javascript for CanCode's Javascript Fundamentals Class Fall '22. First Project of the semester.
 
+
+//
 Change Log:
 11/14/22 -- Initial Commit, package.json created. Created index.js. Copied over word-bank.js
+
+11/28/22 -- Committing basic version of the game. Can't figure out a clean way to draw a hangman, so I've changed it to just letters for now. 
+
+
+//
+Difficulties: 
+1. Drawing a hangman in console that looks like a hangman?
+Uh It's just turning out weird. 
+
+2. I wanted to make the game tell me if I guessed the same letter I had guessed already ... but I confused myself, broke my game, and decided to leave that out. 
+
+3. I would have rather had the game remind me to use letters if I input a symbol or a word or a number, but it just continues on. Since it's not broken, I'm deciding to leave it as is.
+
+
+//
+RESOURCES:
+I've pulled as many links as I could remember researching out of my history and bookmarks and listed them here as frequently as I could remember to do so. 
+
+~ 
+1. These links help understand built in ways to handle random number generation, I think, more or less. 
+  a.) https://www.w3schools.com/jsref/jsref_floor.asp
+  b.) https://www.w3schools.com/jsref/jsref_random.asp
+  c.) https://youtu.be/UZqSpuUJPa0
+
+2. This helped me understand callbacks and for each. 
+  a.) https://www.youtube.com/watch?v=kz_vwAF4NHI
+  b.) https://www.youtube.com/watch?v=SXb5LN_opbA
+  c.) https://www.w3schools.com/js/js_callback.asp
+  d.) https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+  e.) https://www.w3schools.com/jsref/jsref_foreach.asp
+
+3. For a WHILE my game was broken, because I forgot to make a variable for the alphabet... research helped resolve this, and kept me from having to type that all out myself. Do I completely get it? NO. Absolutely not. But it WORKED.
+  a.) https://www.w3resource.com/javascript/form/all-letters-field.php
+  b.) https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+  c.) https://stackoverflow.com/questions/19657750/what-is-the-differnce-between-a-za-z-and-a-za-z
+  d.) https://www.youtube.com/watch?v=rhzKDrUiJVk
+  e.) https://stackoverflow.com/questions/3532053/regular-expression-for-only-characters-a-z-a-z
+  
+4. Understanding the problem by seeing other people break it down. These weren't totally helpful, I still needed to look up a lot more as you'll see with the rest of these links, but still.
+  a.) https://www.youtube.com/watch?v=dgvyE1sJS3Y&t=588s
+  b.) https://www.youtube.com/watch?v=T6uht1A0114
+  c.) https://www.youtube.com/watch?v=A-cLRj-349Y
+  d.) https://codepen.io/cathydutton/pen/JjpxMm
+  e.) https://stackoverflow.com/questions/17688515/stuck-on-my-first-program-game-hangman
+  f.) https://stackoverflow.com/questions/58279238/error-code-coming-up-in-hangman-game-using-javascript
+  g.) https://nostarch.com/download/JS4K_ch7.pdf (Specifically the pseudocode section)
+  h.) https://codesandbox.io/s/z9fhk
+
+5. String method refresher.
+ a.) https://www.youtube.com/watch?v=BZiPQ6DejwA
+
+6. I still am having formatting issues, but this helped a little:
+  a.) https://www.samanthaming.com/tidbits/58-multi-line-strings-with-template-literals/
--- index.js
+++ index.js
@@ -1,7 +1,155 @@
 import prompt from "readline-sync";
 import wordBank from "./word-bank.js";
 
+// Variables to track wins vs losses
+let wins = 0;
+let losses = 0;
 
-let randomNumber = Math.round(Math.random() * wordBank.length)
+/* Here's how the game might reset after playing a round, and also creates a variable or two for some arrays we will need. */
+const resetGame = () => {
+  let answerList = [];
+  let guesses = 7;
+  let letterList = [];
 
-console.log('Word of the day:', wordBank[randomNumber])
+  /* This should be the game itself. Here it's using the wordbank file to to pick a random word (math.floor/math.random) */
+ const gameOfHangman = () => {
+  const word = wordBank[Math.floor(Math.random() * wordBank.length)];
+  const letters = word.split("");
+  let lettersLeft = word.length;
+  
+  const getAnswerList = () => {
+    letters.forEach((_, index) => {
+      answerList[index] = "_"
+    });
+  };
+    
+  getAnswerList();
+    
+  console.log(`
+  Let's Play Hangman!
+  Press ctrl+c to stop`);
+    
+  while (lettersLeft > 0 && guesses > 0) {
+    console.log(answerList.join(" "));
+    let guess = prompt.question(`
+    Try and guess a letter: `).toLowerCase();
+
+
+    /* In this game, the ideal input is a string which is one letter. If it's anything else, we want the game to keep going. 
+    Instead of using the alphabet as a variable, we're using regular expressions. See readme for my sources because I don't *really* get it either lol */     
+      
+    const checkInput = () => {
+      if (guess === "") {
+        console.log(`
+        Please type in a letter.`)
+      } else if (/[^a-zA-Z]/.test(guess[0])) {
+        console.log(`
+        Please type in a letter.`)
+      } else if (/[a-zA-Z]/.test(guess[0])) {
+        letters.forEach((letter, index) => {
+          if (guess[0] === letter) {
+            if (answerList[index] === "_") {
+              answerList[index] = guess[0];
+              lettersLeft--;
+            };
+          };
+        });
+        if (!letterList.includes(guess[0]) && !letters.includes(guess[0])) {
+          guesses--
+        };
+        if (!letterList.includes(guess[0])) {
+          letterList.push(guess[0]);
+        };
+      };
+    };
+
+    checkInput();
+
+    /* This, in theory, should write H A N G M A N in console as the player guess letters, and should also let the player know how many more times they can make a guess. */
+    const gameDraw = () => {
+      if (guesses === 7) {
+        console.log(`
+        You've got ${guesses} lives left.`)
+      } else if (guesses === 6) {
+        console.log(`H 
+        `);
+        console.log(`
+        You've got ${guesses} lives left.`)
+      } else if (guesses === 5) {
+        console.log(`H A 
+        `)
+        console.log(`
+        You've got ${guesses} lives left.`)
+      } else if (guesses === 4) {
+        console.log(`H A N 
+        `)
+        console.log(`
+        You've got ${guesses} lives left.`)
+      } else if (guesses === 3) {
+        console.log(`H A N G 
+        `)
+        console.log(`
+        You've got ${guesses} lives left.`)
+      } else if (guesses === 2) {
+        console.log(`H A N G M 
+        `)
+        console.log(`
+        You've got ${guesses} lives left.`)
+      } else if (guesses === 1) {
+        console.log(`H A N G M A 
+        `)
+        console.log(`
+        You've got ${guesses} lives left.`)
+      } else if (guesses === 0) {
+        console.log(`H A N G M A N
+        `)
+        console.log(`
+        You've got ${guesses} lives left.`)
+      };
+      console.log(`You've tried these letters so far: ${letterList}.
+      `);
+    };
+
+    gameDraw();
+    
+  };
+
+  /* This should show a message if the answer was either wrong or right and update the counters so the player can see. Maybe. Probably. */
+  const showResults = () => {
+    if (lettersLeft > 0) {
+      console.log(answerList.join(" "));
+      console.log(`
+      
+      HANGMAN!
+      You're out of lives.
+      The word you were trying to guess is: ${word}.
+      `)
+      losses++;
+    } else {
+      console.log(answerList.join(" "));
+      console.log(`
+      
+      You survived!
+      The answer was ${word}.
+      `)  
+       wins++;
+    };
+    console.log(`
+    You survived ${wins} times and have lost ${losses} times.
+    `)
+  };
+    
+  showResults();
+  
+};
+  
+gameOfHangman();
+};
+
+//This should allow another reset after each game.
+
+while(true) {
+  resetGame();
+};
+
+/* This maybe isn't the cleanest code ever but when I typed in node index.js and IT RAN without a wall of errors, it was a really cool feeling. Do I get it? Sorta. Do I wish I really truly UNDERSTOOD all of it? Maybe. Am I glad this thing is done? Absolutely. (: */
